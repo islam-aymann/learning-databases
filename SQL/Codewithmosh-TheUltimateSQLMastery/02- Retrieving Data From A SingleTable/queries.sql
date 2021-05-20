55153ce35c42392684891a4e14c3f627fdd13772
Islam Ayman <e-i.mohammed@lean.sa>
2021-05-20 21:35:53 +0200

    Initial commit

---
+++ SQL/Codewithmosh-TheUltimateSQLMastery/02- Retrieving Data From A SingleTable/queries.sql
@@ -0,0 +1,16 @@
+USE sql_store;
+
+SELECT *
+FROM customers
+-- WHERE customer_id = 1
+ORDER BY first_name;
+
+SELECT first_name, last_name
+FROM customers;
+
+SELECT first_name,
+       last_name,
+       points,
+       points * 10 + 100 AS result
+FROM customers;
+
